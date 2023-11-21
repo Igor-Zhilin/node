405476109bf585c58cfacd4dafeeaa958f92b08f
Igor-Zhilin <izilin33@gmail.com>
2023-11-21 09:42:59 +0300

    commit

--- app.js
+++ app.js
@@ -1,77 +1,101 @@
 const express = require("express");
 const favicon = require("express-favicon");
-const path = require("path");
 const fs = require("fs");
-const app = express("");
+const path = require("path");
+const { nextTick } = require("process");
 const ejs = require("ejs");
 
+const app = express();
+const port = "3000";
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "views")));
 
-app.use(express.json());
-app.use(express.urlencoded({ extendend: true }));
+const routeTest = "/test";
+const routeSlash = "/";
+const filePath = path.join(__dirname, "tmp", "1.txt");
 
-// console.log(dirname + "/public/favicon.png");
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(favicon(__dirname + "/public/favicon.png"));
-
-const port = "3000";
-app.get("/test", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-  addLine("Пинганули /");
+fs.writeFile(filePath, `Сервер запущен. Порт: ${port}`, (err) => {
+  if (err) console.error(err);
+  console.log("файл создан");
 });
 
-app.get("/test", (req, res) => {
-  console.log("Прошли по пути тест");
-  res.end("Hello");
-});
-
-app.post("/test", (req, res) => {
-  console.log("Прошли по пути post test");
-  console.log(req.body);
-  res.end("Прошли по пути post test");
-});
-app.listen(port, function () {
-  console.log("Сервер запущен порт " + port);
-  addLine("server started ");
-});
-
-function addLine(line) {
-  line = line + "timestamp:" + new Date().toLocaleString();
+function logger(port, router) {
   fs.appendFile(
-    path.join(__dirname + "/public/logger.txt"),
-    line + "\n",
+    filePath,
+    `\nЛогируем ping по адресу localhost:${port}${router}. Время: ${new Date()}`,
     (err) => {
-      if (err) console.log(err);
+      if (err) console.error(err);
+      console.log("файл переписан");
     }
   );
 }
 
+console.log(app.get("env"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
+app.use(
+  "css/bootstrap-5.3.2",
+  express.static(
+    path.join(__dirname, "css/bootstrap-5.3.2/dist/css/bootstrap.min.css")
+  )
+);
+app.use(favicon(__dirname + "/public/favicon.ico"));
+
+app.get(routeTest, (req, res) => {
+  logger(port, routeTest);
+  res.end("/test");
+});
+app.post(routeTest, (req, res) => {
+  console.log("Прошли по пути post/test");
+  logger(port, routeTest);
+  res.end("post/test");
+});
+console.log(__dirname + "/public/favicon.ico");
+app.get(routeSlash, function (req, res) {
+  logger(port, routeSlash);
+  res.end();
+});
+app.get(routeSlash, function (req, res) {
+  logger(port, routeSlash);
+  res.end("/");
+});
+app.post(routeSlash, function (req, res) {
+  logger(port, routeSlash);
+  res.end("/");
+});
+
+app.get("env") == "production";
+console.log(app.get("env"));
+if (app.get("env") == "production") {
+  app.use((req, res, err) => {
+    res.status(err.status);
+    res.sendFile(err.message);
+  });
+}
+
+app.listen(port, () => {
+  console.log(`listen on port ${port}`);
+});
+
+//ERROR HANDLER
 app.use((req, res, next) => {
-  const err = new Error("Couldn't get path");
+  const err = new Error("Could't get path");
   err.status = 404;
   next(err);
 });
 
-console.log(app.get("env"));
 if (app.get("env") != "development") {
   app.use(function (err, req, res, next) {
+    console.log(err.status, err.message);
     res.status = 404;
-    let link = "https://edible-printing.ru/wa-data/public/shop/products/57/25/2557/images/3901/3901.400.jpg"
-    res.render("error.ejs", {err, link});
+    link = "https://centralsib.com/media/gallery/kukushka.jpg";
+    res.render("error.ejs", { err, link });
   });
 } else {
   app.use(function (err, req, res, next) {
     console.log(app.get("env"), err.status, err.message);
-  });
-}
-
-app.get("env") == "production";
-if (app.get("env") == "production") {
-  app.use((err, req, res) => {
-    res.status(err.status);
-    res.sendFile(err.message);
   });
 }
