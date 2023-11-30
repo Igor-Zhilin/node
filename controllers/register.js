b072d744c08d6013baf37209d8d155600f2529a1
Igor-Zhilin <izilin33@gmail.com>
2023-11-30 15:24:17 +0300

    commit

--- controllers/register.js
+++ controllers/register.js
@@ -1,3 +1,13 @@
+const User = require("../models/user");
+
 exports.form = (req, res) => {
   res.render(`register`, {});
+};
+
+exports.submit = (req, res, next) => {
+const email = req.body.user.email;
+
+if(!Игорь в базе данных есть?)
+
+  User.create(req.body.user, cb)
 };
